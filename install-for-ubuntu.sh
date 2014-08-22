b95abda21895ec22ffcdcef15075db7e64d92d39
long-long-float <niinikazuki@yahoo.co.jp>
2014-08-22 20:37:09 +0900

    ubuntu向けのインストールスクリプトを追加

---
+++ install-for-ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# install fish shell
+sudo apt-add-repository -y ppa:fish-shell/release-2
+sudo apt-get update
+sudo apt-get -y install fish
+
+echo "installed fish shell"
+
+# install rbenv
+sudo apt-get -y install build-essential bison libreadline6-dev curl git-core zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libncurses5-dev
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+echo "installed rbenv"
