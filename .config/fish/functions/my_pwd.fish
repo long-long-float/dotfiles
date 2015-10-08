17c2d17e198992de82800849e1dc8065aef25682
long-long-float <niinikazuki@yahoo.co.jp>
2015-10-08 14:19:11 +0900

    my_pwdの引数を削除した

--- .config/fish/functions/my_pwd.fish
+++ .config/fish/functions/my_pwd.fish
@@ -1,4 +1,4 @@
-function my_pwd -V limit
+function my_pwd
   set -l realhome ~
   echo $PWD | sed -e "s|^$realhome\$|~|" -e "s|^$realhome/|~/|" -e 's-\([^/.]\)[^/]*/-\1/-g' -e "s-\([^/.]\{10\}\)[^/]*\$-\1...-"
 end
